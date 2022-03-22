3ab5ab0b8fda285b55ecd5204799e52f468b6ffd
VanThuong20499 <100428178+VanThuong20499@users.noreply.github.com>
2022-03-23 00:40:46 +0700

    update

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+var img = document.querySelector('.booking__book-now-img');
+var menuItemBtn = document.querySelector('.header__menu');
+var menuItem = document.querySelector('.menu-item');
+var closeBtn = document.querySelector('.menu-item-header-icon')
+function sleep(ms){
+    return new Promise(function(resolve){
+        setTimeout(resolve, ms);
+    })
+}
+
+function repeat(){
+    sleep(3000)
+        .then(function(){
+            img.src = "./assets/img/package-3.jpg";
+            return sleep(3000);
+        })
+        .then(function(){
+            img.src = "./assets/img/parallax-4.jpg";
+            return sleep(3000);
+        })
+        .then(function(){
+            img.src = "./assets/img/parallax-4-1.jpg";
+            return repeat();
+        })
+}
+repeat()
+
+menuItemBtn.addEventListener('click', function(){
+    menuItem.classList.add('open');
+})
+
+closeBtn.addEventListener('click', function(){
+    menuItem.classList.remove('open');
+})
